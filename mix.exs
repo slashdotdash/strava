ed3eff7e50df317ec25ebac4075683da9a1b96a8
Ben Smith <ben@10consulting.com>
2016-01-18 22:47:43 +0000

    Add dependency on `httpoison`

--- mix.exs
+++ mix.exs
@@ -1,10 +1,16 @@
 defmodule Strava.Mixfile do
   use Mix.Project
 
+  @description """
+    Elixir wrapper for the Strava API (V3)
+  """
+
   def project do
     [app: :strava,
      version: "0.0.1",
      elixir: "~> 1.2",
+     name: "Strava",
+     description: @description,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -27,7 +33,7 @@ defmodule Strava.Mixfile do
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type "mix help deps" for more examples and options
-  defp deps do
-    []
+  def deps do
+    [{:httpoison, "~> 0.8.1"}]
   end
 end

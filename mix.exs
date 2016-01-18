70cc946ca49b099085258ea2606e782001321933
Ben Smith <ben@10consulting.com>
2016-01-18 22:50:06 +0000

    Add license (MIT)

--- mix.exs
+++ mix.exs
@@ -16,24 +16,12 @@ defmodule Strava.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
      mod: {Strava, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  def deps do
+  defp deps do
     [{:httpoison, "~> 0.8.1"}]
   end
 end

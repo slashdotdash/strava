dd306491430a491caa57f5fde755165825651c1a
Ben Smith <ben@10consulting.com>
2016-10-20 10:13:40 +0100

    Example Strava club from API docs used in unit tests
    Tests run faster as the club has fewer members.

--- test/strava/club_test.exs
+++ test/strava/club_test.exs
@@ -9,35 +9,36 @@ defmodule Strava.ClubTest do
   end
 
   test "retrieve club" do
-    use_cassette "club/retrieve#7289" do
-      club = Strava.Club.retrieve(7289)
+    use_cassette "club/retrieve#1" do
+      club = Strava.Club.retrieve(1)
 
       assert club != nil
-      assert club.name == "VC Venta"
+      assert club.id == 1
+      assert club.name == "Team Strava Cycling"
       assert club.member_count > 1
       assert club.sport_type == "cycling"
     end
   end
 
   test "list members" do
-    use_cassette "club/list_members#7289" do
-      members = Strava.Club.list_members(7289, %Strava.Pagination{per_page: 5, page: 1})
+    use_cassette "club/list_members#1" do
+      members = Strava.Club.list_members(1, %Strava.Pagination{per_page: 5, page: 1})
 
       assert length(members) == 5
     end
   end
 
   test "list members by page" do
-    use_cassette "club/list_members#7289.page#3" do
-      members = Strava.Club.list_members(7289, %Strava.Pagination{per_page: 200, page: 3})
+    use_cassette "club/list_members#1.page#3" do
+      members = Strava.Club.list_members(1, %Strava.Pagination{per_page: 200, page: 3})
 
       assert members == []
     end
   end
 
   test "stream members" do
-    use_cassette "club/stream_members#7289", match_requests_on: [:query]  do
-      member_stream = Strava.Club.stream_members(7289)
+    use_cassette "club/stream_members#1", match_requests_on: [:query]  do
+      member_stream = Strava.Club.stream_members(1)
 
       assert Enum.count(member_stream) > 1
     end

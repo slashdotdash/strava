9bf9351e22e2618baa58bd68e75cbb022807e2d8
Jake Sparling <jacob.sparling@gmail.com>
2018-06-16 19:49:07 -0700

    Add filters to Activity.retrieve

--- lib/strava/util.ex
+++ lib/strava/util.ex
@@ -49,6 +49,12 @@ defmodule Strava.Util do
     struct(destination, map)
   end
 
+  @spec query_string_no_pagination(map) :: binary
+  def query_string_no_pagination(filters) do
+    filters
+    |> URI.encode_query
+  end
+
   @spec query_string(Strava.Pagination.t, map) :: binary
   def query_string(pagination, filters \\ %{}) do
     pagination
